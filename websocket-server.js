42501dd088602d82be15c5fe04c41736107772bc
MGebremski <m.gebremski@steinberg.de>
2024-08-11 23:17:34 +0200

    Initial commit

---
+++ websocket-server.js
@@ -0,0 +1,20 @@
+const WebSocket = require('ws');
+const http = require('http');
+
+const server = http.createServer();
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        // Broadcast the video data to all connected clients
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+});
+
+server.listen(8081, () => {
+    console.log('WebSocket server is running on ws://localhost:8081');
+});
